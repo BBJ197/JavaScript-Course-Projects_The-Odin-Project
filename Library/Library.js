fa0442f1d9c4c341cb3330c8086685fdf31d96da
BBJ197 <beamlakbekele197@gmail.com>
2026-03-25 22:12:12 +0300

    added a function that adds book cards

--- Library/Library.js
+++ Library/Library.js
@@ -16,4 +16,28 @@ function addBookToLibrary(title1, author1, pages1) {
 
 addBookToLibrary("feker", "shems", 77)
 addBookToLibrary("bsb", "uwb9", 97)
-console.log(myLibrary);
+
+function addCard() {
+    const container = document.getElementsByClassName("cards")[0];
+    for (let i=0; i<myLibrary.length; i++){
+        container.insertAdjacentHTML("beforeend", `
+            <div class="card">
+                <div>
+                    <div class="title">${myLibrary[i].title}</div>
+                    <div class="content">
+                    <b>By:</b> ${myLibrary[i].author} <br>
+                    <b>Page:</b> ${myLibrary[i].pages}<br>
+                    <b>Id:</b> ${myLibrary[i].id}         
+                    </div>
+                </div>
+                <div class="icons">
+                    <span class="mdi mdi-star-plus-outline"></span>
+                    <span class="mdi mdi-eye-plus-outline"></span>
+                    <span class="mdi mdi-transit-connection-variant"></span>
+                </div>
+            </div>
+        `);
+    }
+}
+
+addCard();
